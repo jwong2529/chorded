1e23c8d3ab97a622136a17ba3a22731ba33fa04e
jwong2529 <janice.wong2529@gmail.com>
2024-05-23 18:07:17 -0700

    Initial Commit

---
+++ MusicReviewApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicReviewApp
+//
+//  Created by Janice Wong on 5/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicReviewApp/MusicReviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicReviewAppApp.swift
+//  MusicReviewApp
+//
+//  Created by Janice Wong on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicReviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
